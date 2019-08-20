05b5441d27ba1ccc6d31bc45f1ea98fe7fd1f48d
command7 <vijay_saravanan@outlook.com>
2019-08-20 17:05:55 -0400

    Feat: LoadDimensionOperator extracts data from staging tables, transforms and loads it into dimension tables

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.plugins.helpers.sql_queries import SqlQueries
 
 class LoadDimensionOperator(BaseOperator):
 
@@ -8,15 +9,32 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 conn_id,
+                 dest_table_name,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = conn_id
+        self.dest_table_name = dest_table_name
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(f'Extracting data from staging tables to {}'.format(self.dest_table_name))
+        redshift_hook = PostgresHook('redshift_connection')
+        if self.dest_table_name == "users":
+            redshift_hook.run(sql=SqlQueries.user_table_insert)
+            self.logging.info(f"Successfully loaded data into {} table".format(self.dest_table_name))
+        elif self.dest_table_name == 'songs':
+            redshift_hook.run(sql=SqlQueries.song_table_insert)
+            self.logging.info(f"Successfully loaded data into {} table".format(self.dest_table_name))
+        elif self.dest_table_name == 'time':
+            redshift_hook.run(sql=SqlQueries.time_table_insert)
+            self.logging.info(f"Successfully loaded data into {} table".format(self.dest_table_name))
+        elif self.dest_table_name == 'users':
+            redshift_hook.run(sql=SqlQueries.user_table_insert)
+            self.logging.info(f"Successfully loaded data into {} table".format(self.dest_table_name))
+        elif self.dest_table_name == 'artists':
+            redshift_hook.run(sql=SqlQueries.artist_table_insert)
+            self.logging.info(f"Successfully loaded data into {} table".format(self.dest_table_name))
+        else:
+            self.logging.error("Wrong table name")
+            raise ValueError
