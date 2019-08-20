4e611f80ac27248488f3c36991d3524f50cb953a
command7 <vijay_saravanan@outlook.com>
2019-08-20 17:10:45 -0400

    Feat: LoadFactOperator extracts data from staging tables, transforms and loads it into fact tables

--- airflow/plugins/operators/load_fact.py
+++ airflow/plugins/operators/load_fact.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.plugins.helpers.sql_queries import SqlQueries
 
 class LoadFactOperator(BaseOperator):
 
@@ -8,15 +9,16 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 conn_id,
+                 table_name="Not Specified",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = conn_id
+        self.table_name = table_name
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info(f'Extracting data into facts table {}'.format(self.table_name))
+        redshift_hook = PostgresHook(self.conn_id)
+        redshift_hook.run(sql=SqlQueries.songplay_table_insert)
+        self.logging.info(f"Successfully loaded data into {}".format(self.table_name))
