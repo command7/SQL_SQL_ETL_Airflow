12941c2c6a4a6348eb6f5c3e4d8f297f0b1c8da5
command7 <vijay_saravanan@outlook.com>
2019-08-21 15:29:38 -0400

    Misc: Change class variable dest_table_name to table_name

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -10,31 +10,31 @@ class LoadDimensionOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  conn_id,
-                 dest_table_name,
+                 table_name,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.conn_id = conn_id
-        self.dest_table_name = dest_table_name
+        self.table_name = table_name
 
     def execute(self, context):
-        self.logging.info(f'Extracting data from staging tables to {self.dest_table_name}')
+        self.logging.info(f'Extracting data from staging tables to {self.table_name}')
         redshift_hook = PostgresHook(self.conn_id)
-        if self.dest_table_name == "users":
-            redshift_hook.run(sql=SqlQueries.user_table_insert.format(self.dest_table_name))
-            self.logging.info(f"Successfully loaded data into {self.dest_table_name} table")
-        elif self.dest_table_name == 'songs':
-            redshift_hook.run(sql=SqlQueries.song_table_insert.format(self.dest_table_name))
-            self.logging.info(f"Successfully loaded data into {self.dest_table_name} table")
-        elif self.dest_table_name == 'time':
-            redshift_hook.run(sql=SqlQueries.time_table_insert.format(self.dest_table_name))
-            self.logging.info(f"Successfully loaded data into {self.dest_table_name} table")
-        elif self.dest_table_name == 'users':
-            redshift_hook.run(sql=SqlQueries.user_table_insert.format(self.dest_table_name))
-            self.logging.info(f"Successfully loaded data into {self.dest_table_name} table")
-        elif self.dest_table_name == 'artists':
-            redshift_hook.run(sql=SqlQueries.artist_table_insert.format(self.dest_table_name))
-            self.logging.info(f"Successfully loaded data into {self.dest_table_name} table")
+        if self.table_name == "users":
+            redshift_hook.run(sql=SqlQueries.user_table_insert.format(self.table_name))
+            self.logging.info(f"Successfully loaded data into {self.table_name} table")
+        elif self.table_name == 'songs':
+            redshift_hook.run(sql=SqlQueries.song_table_insert.format(self.table_name))
+            self.logging.info(f"Successfully loaded data into {self.table_name} table")
+        elif self.table_name == 'time':
+            redshift_hook.run(sql=SqlQueries.time_table_insert.format(self.table_name))
+            self.logging.info(f"Successfully loaded data into {self.table_name} table")
+        elif self.table_name == 'users':
+            redshift_hook.run(sql=SqlQueries.user_table_insert.format(self.table_name))
+            self.logging.info(f"Successfully loaded data into {self.table_name} table")
+        elif self.table_name == 'artists':
+            redshift_hook.run(sql=SqlQueries.artist_table_insert.format(self.table_name))
+            self.logging.info(f"Successfully loaded data into {self.table_name} table")
         else:
             self.logging.error("Wrong table name")
             raise ValueError
