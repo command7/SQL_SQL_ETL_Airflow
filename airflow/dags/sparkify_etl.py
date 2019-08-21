ab6652c6b44b8121fd14b204d2c416c01382c6ff
command7 <vijay_saravanan@outlook.com>
2019-08-21 17:51:05 -0400

    Feat: Define workflow order in sparkify_etl.py

--- airflow/dags/sparkify_etl.py
+++ airflow/dags/sparkify_etl.py
@@ -7,8 +7,6 @@ from airflow.operators.sparkify_plugin import (StageToRedshiftOperator,
                                LoadDimensionOperator,
                                DataQualityOperator)
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     'owner': 'udacity',
@@ -82,3 +80,19 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
+
