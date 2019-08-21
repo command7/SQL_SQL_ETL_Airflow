9d342ad2657d8a2d35982507ae4444d5eb229c19
command7 <vijay_saravanan@outlook.com>
2019-08-21 18:33:34 -0400

    Config: Disable catchup

--- airflow/dags/sparkify_etl.py
+++ airflow/dags/sparkify_etl.py
@@ -20,7 +20,8 @@ default_args = {
 dag = DAG('Songs_Events_ETL',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='0 * * * *',
+          catchup=False
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
