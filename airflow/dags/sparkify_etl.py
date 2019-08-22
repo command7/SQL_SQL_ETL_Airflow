5eca4e0e858b6d907ffc9d55e0d540599c247d25
command7 <vijay_saravanan@outlook.com>
2019-08-21 20:08:38 -0400

    Dev: Provide connection ID and table names as parameters for Run_data_quality_checks task

--- airflow/dags/sparkify_etl.py
+++ airflow/dags/sparkify_etl.py
@@ -82,7 +82,15 @@ load_time_dimension_table = LoadDimensionOperator(
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    conn_id='redshift_connection',
+    table_names=[
+        'users',
+        'time',
+        'artists',
+        'songs',
+        'songplays'
+    ]
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
