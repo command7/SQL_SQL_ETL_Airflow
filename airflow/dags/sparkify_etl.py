88f7593c57ac236d7f3815d8c7ad66770e45bd52
command7 <vijay_saravanan@outlook.com>
2019-08-28 17:40:27 -0400

    Dev: Comment out everything except data quality operator

--- airflow/dags/sparkify_etl.py
+++ airflow/dags/sparkify_etl.py
@@ -24,61 +24,61 @@ dag = DAG('Songs_Events_ETL',
           schedule_interval='0 * * * *',
           catchup=False)
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
-
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag,
-    conn_id='redshift_connection',
-    sql_template='create_tables.sql',
-    s3_location='s3://udacity-dend/log_data',
-    table_name="staging_events",
-    json_path='s3://udacity-dend/log_json_path.json'
-)
-
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag,
-    conn_id='redshift_connection',
-    sql_template='create_tables.sql',
-    s3_location='s3://udacity-dend/song_data',
-    table_name="staging_songs"
-)
-
-load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag,
-    conn_id='redshift_connection',
-    table_name='songplays'
-)
-
-load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag,
-    conn_id='redshift_connection',
-    table_name='users'
-)
-
-load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag,
-    conn_id='redshift_connection',
-    table_name='songs'
-)
-
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag,
-    conn_id='redshift_connection',
-    table_name='artists'
-)
-
-load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag,
-    conn_id='redshift_connection',
-    table_name='time'
-)
+# start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+#
+# stage_events_to_redshift = StageToRedshiftOperator(
+#     task_id='Stage_events',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     sql_template='create_tables.sql',
+#     s3_location='s3://udacity-dend/log_data',
+#     table_name="staging_events",
+#     json_path='s3://udacity-dend/log_json_path.json'
+# )
+#
+# stage_songs_to_redshift = StageToRedshiftOperator(
+#     task_id='Stage_songs',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     sql_template='create_tables.sql',
+#     s3_location='s3://udacity-dend/song_data',
+#     table_name="staging_songs"
+# )
+#
+# load_songplays_table = LoadFactOperator(
+#     task_id='Load_songplays_fact_table',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     table_name='songplays'
+# )
+#
+# load_user_dimension_table = LoadDimensionOperator(
+#     task_id='Load_user_dim_table',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     table_name='users'
+# )
+#
+# load_song_dimension_table = LoadDimensionOperator(
+#     task_id='Load_song_dim_table',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     table_name='songs'
+# )
+#
+# load_artist_dimension_table = LoadDimensionOperator(
+#     task_id='Load_artist_dim_table',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     table_name='artists'
+# )
+#
+# load_time_dimension_table = LoadDimensionOperator(
+#     task_id='Load_time_dim_table',
+#     dag=dag,
+#     conn_id='redshift_connection',
+#     table_name='time'
+# )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
@@ -96,17 +96,17 @@ run_quality_checks = DataQualityOperator(
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
 
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift
-stage_events_to_redshift >> load_songplays_table
-stage_songs_to_redshift >> load_songplays_table
-load_songplays_table >> load_song_dimension_table
-load_songplays_table >> load_user_dimension_table
-load_songplays_table >> load_artist_dimension_table
-load_songplays_table >> load_time_dimension_table
-load_song_dimension_table >> run_quality_checks
-load_user_dimension_table >> run_quality_checks
-load_artist_dimension_table >> run_quality_checks
-load_time_dimension_table >> run_quality_checks
+# start_operator >> stage_events_to_redshift
+# start_operator >> stage_songs_to_redshift
+# stage_events_to_redshift >> load_songplays_table
+# stage_songs_to_redshift >> load_songplays_table
+# load_songplays_table >> load_song_dimension_table
+# load_songplays_table >> load_user_dimension_table
+# load_songplays_table >> load_artist_dimension_table
+# load_songplays_table >> load_time_dimension_table
+# load_song_dimension_table >> run_quality_checks
+# load_user_dimension_table >> run_quality_checks
+# load_artist_dimension_table >> run_quality_checks
+# load_time_dimension_table >> run_quality_checks
 run_quality_checks >> end_operator
 
